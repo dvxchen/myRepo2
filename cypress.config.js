2765480d78b1c10e6535452cd106aed95d8a80ff
dvxchen <davy.chen@sap.com>
2026-03-25 16:34:34 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: process.env.CYPRESS_CASENAME,
+    specPattern: "cypress/e2e/**/*.cy.js"， //process.env.CYPRESS_CASENAME,
     video: false,
     // 关键：禁用支持文件，消除提示
     supportFile: false
